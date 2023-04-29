ea418bc866c526118fcc86922cf3a01dc24b2664
teerawat-cc <terawat.cc@gmail.com>
2023-04-30 02:19:52 +0700

    update asm2

---
+++ computer organization/test2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i, space, rows, k = 0;
+    printf("Enter the nummber of rows: ");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; ++i, k = 0) {
+        for (space = 1; space <= rows - i; ++space){
+            /* code */
+        }
+        
+    }
+}
